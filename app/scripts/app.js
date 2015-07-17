bdc0e4d6548c22cc1b18409a89a0f8d4b46cea64
Ameya Thakur <redsupercar@gmail.com>
2015-07-16 20:34:19 -0700

    Initialize with yeoman angular, no sass, no bootstrap, default angular packages

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name teamManagerApp
+ * @description
+ * # teamManagerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('teamManagerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
