4faf9f9f5fb28dd36d36c0fd8fce6ab28c24182b
Ameya Thakur <redsupercar@gmail.com>
2015-08-10 22:20:58 -0700

    install firebase and angularfire

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,8 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-material/angular-material.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/firebase/firebase.js',
+      'bower_components/angularfire/dist/angularfire.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
