3a3d6a52cbf217507a3b0b298f48f682b853145a
Ameya Thakur <redsupercar@gmail.com>
2015-12-22 20:58:54 +0000

    sync

--- app/scripts/controllers/jobs.js
+++ app/scripts/controllers/jobs.js
@@ -11,5 +11,7 @@ angular.module('teamManagerApp')
   .controller('JobsController', function ($scope, $firebaseObject) {
     var ref = new Firebase("https://phsrobotics.firebaseio.com/jobs");
     $scope.high = $firebaseObject(ref.child('high'));
+    $scope.mid = $firebaseObject(ref.child('mid'));
+    $scope.low = $firebaseObject(ref.child('low'));
     $scope.test = 'a test';
   });
